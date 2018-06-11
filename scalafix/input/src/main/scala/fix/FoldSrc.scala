b3262cecd246233f9ae31ca550a210e205fe8064
Guillaume Massé <masgui@gmail.com>
2018-06-11 15:54:15 +0200

    Rule for symbolic foldLeft(/:) and foldRight(:/) (fix #27)

---
+++ scalafix/input/src/main/scala/fix/FoldSrc.scala
@@ -0,0 +1,12 @@
+/*
+rule = "scala:fix.Scalacollectioncompat_NewCollections"
+ */
+package fix
+
+class FoldSrc(xs: List[Int]){
+  val f: (Int, Int) => Int = (x, y) => x + y
+  val g: (Int, Int) => Int = (x, y) => x * y
+
+  (0 /: xs)(f)
+  (xs :\ 1)(g)
+}
