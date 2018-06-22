96e955155c1ce1ad5f9066f685879a53e3ce9659
Guillaume Massé <masgui@gmail.com>
2018-06-22 17:17:22 +0200

    Rewrite `mutable.Set/Map` no longer have a `+` operation

---
+++ scalafix/input/src/main/scala/fix/MutSetMapSrc.scala
@@ -0,0 +1,11 @@
+/*
+rule = "scala:fix.Scalacollectioncompat_newcollections"
+ */
+package fix
+
+import scala.collection.mutable
+
+class MutSetMapSrc(map: mutable.Map[Int, Int], set: mutable.Set[Int]) {
+  set + 2
+  map + (2 -> 3)
+}
---
+++ scalafix/output/src/main/scala/fix/MutSetMapSrc.scala
@@ -0,0 +1,11 @@
+
+
+
+package fix
+
+import scala.collection.mutable
+
+class MutSetMapSrc(map: mutable.Map[Int, Int], set: mutable.Set[Int]) {
+  set.clone() += 2
+  map.clone() += (2 -> 3)
+}
