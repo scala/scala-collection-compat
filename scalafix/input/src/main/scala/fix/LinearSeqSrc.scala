be0b88f6e19c0bca36cf65e5a71130ec8962db6b
Guillaume Massé <masgui@gmail.com>
2018-06-25 14:13:58 +0200

    Rewrite LinearSeq -> List

---
+++ scalafix/input/src/main/scala/fix/LinearSeqSrc.scala
@@ -0,0 +1,10 @@
+/*
+rule = "scala:fix.Scalacollectioncompat_newcollections"
+ */
+package fix
+
+import scala.collection.LinearSeq
+
+object LinearSeqSrc {
+  LinearSeq(1)
+}
---
+++ scalafix/output/src/main/scala/fix/LinearSeqSrc.scala
@@ -0,0 +1,10 @@
+
+
+
+package fix
+
+import scala.collection.immutable.List
+
+object LinearSeqSrc {
+  List(1)
+}
