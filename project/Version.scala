e71dc9714f3b6e223f88b266dbbc074f3c255ed8
Seth Tisue <seth@tisue.net>
2019-02-07 17:33:39 -0800

    handle JDK 11's format of java.specification.version
    it's just a lone integer

--- project/Version.scala
+++ project/Version.scala
@@ -7,14 +7,19 @@ object Version {
   private val versionRegex0 = "v?([0-9]+)\\.([0-9]+)\\.([0-9]+)".r
   private val versionRegex1 = "v?([0-9]+)\\.([0-9]+)\\.([0-9]+)-M([0-9]+)".r
   private val versionRegex2 = "([0-9]+)\\.([0-9]+)".r
+  private val versionRegex3 = "([0-9]+)".r
   def parse(raw: String): Option[Version] = {
     raw match {
       case versionRegex0(major, minor, patch) =>
         Some(Version(major.toInt, minor.toInt, patch.toInt))
       case versionRegex1(major, minor, patch, _) =>
         Some(Version(major.toInt, minor.toInt, patch.toInt))
-      case versionRegex2(major, minor) => Some(Version(major.toInt, minor.toInt, 0))
-      case _                           => None
+      case versionRegex2(major, minor) =>
+        Some(Version(major.toInt, minor.toInt, 0))
+      case versionRegex3(major) =>
+        Some(Version(major.toInt, 0, 0))
+      case _ =>
+        None
     }
   }
 }
