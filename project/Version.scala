32687f81d044ad00d0e8ff455583487c5c922a4b
Seth Tisue <seth@tisue.net>
2019-06-02 19:37:35 +0200

    minor code improvement in build (#213)

--- project/Version.scala
+++ project/Version.scala
@@ -5,19 +5,19 @@ case class Version(major: Int, minor: Int, patch: Int) {
 
 object Version {
   // the (#.+)? part allows republishing for a new Scala version
-  private val versionRegex0 = "v?([0-9]+)\\.([0-9]+)\\.([0-9]+)(#.+)?".r
-  private val versionRegex1 = "v?([0-9]+)\\.([0-9]+)\\.([0-9]+)-(.+)(#.+)?".r
-  private val versionRegex2 = "([0-9]+)\\.([0-9]+)(#.+)?".r
-  private val versionRegex3 = "([0-9]+)(#.+)?".r
+  private val versionRegex0 = "v?([0-9]+)\\.([0-9]+)\\.([0-9]+)(?:#.+)?".r
+  private val versionRegex1 = "v?([0-9]+)\\.([0-9]+)\\.([0-9]+)-(.+)(?:#.+)?".r
+  private val versionRegex2 = "([0-9]+)\\.([0-9]+)(?:#.+)?".r
+  private val versionRegex3 = "([0-9]+)(?:#.+)?".r
   def parse(raw: String): Option[Version] = {
     raw match {
-      case versionRegex0(major, minor, patch, _) =>
+      case versionRegex0(major, minor, patch) =>
         Some(Version(major.toInt, minor.toInt, patch.toInt))
-      case versionRegex1(major, minor, patch, _, _) =>
+      case versionRegex1(major, minor, patch, _) =>
         Some(Version(major.toInt, minor.toInt, patch.toInt))
-      case versionRegex2(major, minor, _) =>
+      case versionRegex2(major, minor) =>
         Some(Version(major.toInt, minor.toInt, 0))
-      case versionRegex3(major, _) =>
+      case versionRegex3(major) =>
         Some(Version(major.toInt, 0, 0))
       case _ =>
         None
