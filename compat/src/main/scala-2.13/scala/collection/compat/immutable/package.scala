59c6cfb8463df624fa8ae15be76482d1c6549b62
NthPortal <nthportal@gmail.com>
2020-01-11 16:41:19 -0500

    Backport LazyList

--- compat/src/main/scala-2.13/scala/collection/compat/immutable/package.scala
+++ compat/src/main/scala-2.13/scala/collection/compat/immutable/package.scala
@@ -15,4 +15,7 @@ package scala.collection.compat
 package object immutable {
   type ArraySeq[+T] = scala.collection.immutable.ArraySeq[T]
   val ArraySeq = scala.collection.immutable.ArraySeq
+
+  type LazyList[+T] = scala.collection.immutable.LazyList[T]
+  val LazyList = scala.collection.immutable.LazyList
 }
