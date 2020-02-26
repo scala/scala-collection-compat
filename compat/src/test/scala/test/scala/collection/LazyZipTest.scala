730e6d8b9969ea7e116a7551f13cf95143a08aef
Pietari Kettunen <pietari@sqooba.io>
2020-02-26 12:35:36 +0100

    test for lazy evaluation

--- compat/src/test/scala/test/scala/collection/LazyZipTest.scala
+++ compat/src/test/scala/test/scala/collection/LazyZipTest.scala
@@ -1,6 +1,8 @@
 package test.scala.collection
 
 import org.junit.Assert.assertEquals
+import org.junit.Assert.assertTrue
+import org.junit.Assert.assertFalse
 import org.junit.Test
 
 import scala.collection.compat._
@@ -18,7 +20,6 @@ class LazyZipTest {
 
 	@Test
 	def lazyZipTest(): Unit = {
-
 		val res: List[(Int, Int)] = zipped2.map((a, b) => (a, b))
 		assertEquals(List((1, 1), (2, 2), (3, 3)), res)
 	}
@@ -26,7 +27,19 @@ class LazyZipTest {
 	@Test
 	def lazyZip3_map(): Unit = {
 		val res: List[(Int, Int, String)] = zipped3.map((a: Int, b: Int, c: String) => (a, b, c))
-
 		assertEquals(List((1, 1, "a"), (2, 2, "b"), (3, 3, "c")), res)
 	}
+
+	@Test
+	def collectionValueIsNotEvaluated(): Unit = {
+		val st = Stream.cons(1, throw new AssertionError("should not be evaluated"))
+		ws.lazyZip(st)
+	}
+
+	@Test
+	def zip3collectionValueIsNotEvaluated(): Unit = {
+		val st = Stream.cons(1, throw new AssertionError("should not be evaluated"))
+		ws.lazyZip(st).lazyZip(st)
+	}
+
 }
