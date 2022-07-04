96a4b8c34cc33d99433fbfafd3ccaa3a41fe1cad
Nikita Pedorich <pedorich.n@gmail.com>
2022-07-04 12:22:31 +0200

    Copyright added; Test reworked

--- compat/src/test/scala/test/scala/collection/OptionTest.scala
+++ compat/src/test/scala/test/scala/collection/OptionTest.scala
@@ -1,3 +1,15 @@
+/*
+ * Scala (https://www.scala-lang.org)
+ *
+ * Copyright EPFL and Lightbend, Inc.
+ *
+ * Licensed under Apache License 2.0
+ * (http://www.apache.org/licenses/LICENSE-2.0).
+ *
+ * See the NOTICE file distributed with this work for
+ * additional information regarding copyright ownership.
+ */
+
 package test.scala.collection
 
 import scala.collection.compat._
@@ -6,27 +18,31 @@ import org.junit.Assert._
 
 class OptionTest {
 
+  private val value: String = "example"
+  private val some: Option[String] = Some(value)
+  private val none: Option[String] = None
+
   @Test
   def testWhenTrue: Unit = {
-    val option = Option.when(true)("example")
-    assertEquals(option, Some("example"))
+    val option = Option.when(true)(value)
+    assertEquals(option, some)
   }
 
   @Test
   def testWhenFalse: Unit = {
-    val option = Option.when(false)("example")
-    assertEquals(option, None)
+    val option = Option.when(false)(value)
+    assertEquals(option, none)
   }
 
   @Test
   def testUnlessTrue: Unit = {
-    val option = Option.unless(true)("example")
-    assertEquals(option, None)
+    val option = Option.unless(true)(value)
+    assertEquals(option, none)
   }
 
   @Test
   def testUnlessFalse: Unit = {
-    val option = Option.unless(false)("example")
-    assertEquals(option, Some("example"))
+    val option = Option.unless(false)(value)
+    assertEquals(option, some)
   }
 }
