d77167d25266fd52f9818674fb1075f729a76e26
Seth Tisue <seth@tisue.net>
2022-07-08 15:50:28 -0700

    GitHub Actions config: avoid duplicating Scala version numbers (#546)
    by using new sbt 1.7 feature

--- project/Version.scala
+++ project/Version.scala
@@ -1,24 +1,24 @@
-case class Version(major: Int, minor: Int, patch: Int) {
-  def binary: String            = s"${major}${minor}"
-  override def toString: String = s"${major}.${minor}.${patch}"
+case class Version(major: Int, minor: Int) {
+  override def toString = s"${major}${minor}"
 }
 
 object Version {
-  // the (#.+)? part allows republishing for a new Scala version
-  private val versionRegex0 = "v?([0-9]+)\\.([0-9]+)\\.([0-9]+)(?:#.+)?".r
-  private val versionRegex1 = "v?([0-9]+)\\.([0-9]+)\\.([0-9]+)-(.+)(?:#.+)?".r
+  // `(#.+)?` allows republishing for a new Scala version
+  // `|x` allows the sbt 1.7 style ".x" versions
+  private val versionRegex0 = "v?([0-9]+)\\.([0-9]+)\\.([0-9]+|x)(?:#.+)?".r
+  private val versionRegex1 = "v?([0-9]+)\\.([0-9]+)\\.([0-9]+|x)-(.+)(?:#.+)?".r
   private val versionRegex2 = "([0-9]+)\\.([0-9]+)(?:#.+)?".r
   private val versionRegex3 = "([0-9]+)(?:#.+)?".r
   def parse(raw: String): Option[Version] = {
     raw match {
-      case versionRegex0(major, minor, patch) =>
-        Some(Version(major.toInt, minor.toInt, patch.toInt))
-      case versionRegex1(major, minor, patch, _) =>
-        Some(Version(major.toInt, minor.toInt, patch.toInt))
+      case versionRegex0(major, minor, _) =>
+        Some(Version(major.toInt, minor.toInt))
+      case versionRegex1(major, minor, _, _) =>
+        Some(Version(major.toInt, minor.toInt))
       case versionRegex2(major, minor) =>
-        Some(Version(major.toInt, minor.toInt, 0))
+        Some(Version(major.toInt, minor.toInt))
       case versionRegex3(major) =>
-        Some(Version(major.toInt, 0, 0))
+        Some(Version(major.toInt, 0))
       case _ =>
         None
     }
