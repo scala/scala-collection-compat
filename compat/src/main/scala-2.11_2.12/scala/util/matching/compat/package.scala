956249b46bdb593d40824c96d700e597e34ab1db
Eugene Platonov <jozic@live.com>
2023-05-27 23:34:05 -0400

    Add `RegexOps`

---
+++ compat/src/main/scala-2.11_2.12/scala/util/matching/compat/package.scala
@@ -0,0 +1,30 @@
+/*
+ * Scala (https://www.scala-lang.org)
+ *
+ * Copyright EPFL and Lightbend, Inc.
+ *
+ * Licensed under Apache License 2.0
+ * (http://www.apache.org/licenses/LICENSE-2.0).
+ *
+ * See the NOTICE file distributed with this work for
+ * additional information regarding copyright ownership.
+ */
+
+package scala.util.matching
+
+package object compat {
+  final implicit class RegexOps(val regex: Regex) extends AnyVal {
+
+    /** Returns whether this `Regex` matches the given character sequence.
+     *
+     * Like the extractor, this method takes anchoring into account.
+     *
+     * @param source The text to match against
+     * @return true if and only if `source` matches this `Regex`.
+     * @see [[Regex#unanchored]]
+     * @example {{{"""\d+""".r matches "123" // returns true}}}
+     */
+    def matches(source: CharSequence): Boolean = regex.pattern.matcher(source).matches()
+  }
+
+}
---
+++ compat/src/main/scala-2.13/scala/util/matching/compat/package.scala
@@ -0,0 +1,18 @@
+/*
+ * Scala (https://www.scala-lang.org)
+ *
+ * Copyright EPFL and Lightbend, Inc.
+ *
+ * Licensed under Apache License 2.0
+ * (http://www.apache.org/licenses/LICENSE-2.0).
+ *
+ * See the NOTICE file distributed with this work for
+ * additional information regarding copyright ownership.
+ */
+
+package scala.util.matching
+
+package object compat {
+  type Regex = scala.util.matching.Regex
+  val Regex = scala.util.matching.Regex
+}
